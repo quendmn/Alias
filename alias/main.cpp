22eb620791a0ed21fc58eb6900ea967e5723794c
Chernyh Pauline <xanolaaa@gmail.com>
2023-07-30 12:55:04 +0500

    готовое главное меню, выбор режима

--- alias/main.cpp
+++ alias/main.cpp
@@ -6,80 +6,7 @@
 using namespace std;
 using namespace sf;
 
-// класс для текста
-/*class text_two
-{
-private:
-	Text mtext;  //объект текста
-	float xpos;
-	float ypos;
-	String str; // сам текст
-	int size_font = 48;
-	Color menu_text_color = Color::Black;
-	Font font;
-public:
-	text_two(Text& mtext, float xpos, float ypos, String str, int size_font,
-		Color menu_text_color, Font font){
-	
-		font.loadFromFile("font/Forum.ttf");
-		mtext.setFont(font);
-		mtext.setCharacterSize(size_font);
-		mtext.setPosition(xpos, ypos);
-		mtext.setString(str);
-		mtext.setFillColor(menu_text_color);
-		
-	}
-	//текст будет следить за спрайтом
-	virtual void sleditForSprite(Sprite& s, float x, float y) {
-
-		mtext.setPosition(s.getPosition().x + x, s.getPosition().y + y);
-
-	}
-
-	//дать число тексту
-	void getChislo(float n) {
-
-		ostringstream chislo;
-
-		chislo << n;
-
-		mtext.setString(str + chislo.str());
-
-	}
-
-	//ф-я для текста
-	void setString(string name) {
-
-		mtext.setString(name);
-
-	}
-
-	virtual void setPosition(float x, float y) {
-
-		mtext.setPosition(x, y);
-
-	}
-
-	void setFillTextColor(float R, float G1, float B) {
-
-		mtext.setFillColor(Color(R, G1, B));
-
-	}
-
-	//поменять размер шрифта
-	void setCharacterSize(float a) {
-
-		mtext.setCharacterSize(a);
-
-	}
-
-	//нарисовать в окне
-	virtual void draw(RenderWindow& window) {
-
-		window.draw(mtext);
-
-	};
-};*/
+// класс текста
 class text {
 private:
 
@@ -275,26 +202,50 @@ public:
 
 int main()
 {
-	// создание окна #1
+	// создание окна #1 (главное меню)
 	RenderWindow window(sf::VideoMode(1440, 900), "Alias");
 
 	RectangleShape background(Vector2f(1440, 900));
-	Texture main_window;
-	if (!main_window.loadFromFile("images/main_window.png")) return 4;
-	background.setTexture(&main_window);
+	Texture window_background;
+	if (!window_background.loadFromFile("images/main_window.png")) return 4;
+	background.setTexture(&window_background);
 
-	// создание кнопки главного экрана
+	// создание кнопки Play (::главное меню)
 	Button play_b(500, 70, "                  Play");
 	play_b.setFillRacktengelColor(202, 189, 233);
 	play_b.setFillTextColor(0, 0, 0);
 	play_b.setPosition(835, 611);
 	play_b.setCharacterSize(48);
 
+	// создание кнопки Rules (::главное меню)
 	Button rules_b(500, 70, "                 Rules");
 	rules_b.setFillRacktengelColor(202, 189, 233);
 	rules_b.setFillTextColor(0, 0, 0);
 	rules_b.setPosition(835, 718);
 	rules_b.setCharacterSize(48);
+	
+	// создание кнопки Back 
+	Button back_b(105, 55, "Back");
+	back_b.setFillRacktengelColor(149, 165, 58);
+	back_b.setFillTextColor(255, 255, 221);
+	back_b.setPosition(2000, 2000);
+	back_b.setCharacterSize(48);
+
+	// создание кнопки режимa с игроками
+	Button playermode_b(805, 155, "Player vs player");
+	playermode_b.setFillRacktengelColor(149, 165, 58);
+	playermode_b.setFillTextColor(255, 255, 221);
+	playermode_b.setPosition(2000, 2000);
+	playermode_b.setCharacterSize(48);
+
+	// создание кнопки режимa с пк
+	Button pcmode_b(805, 155, "Player vs pc");
+	pcmode_b.setFillRacktengelColor(149, 165, 58);
+	pcmode_b.setFillTextColor(255, 255, 221);
+	pcmode_b.setPosition(2000, 2000);
+	pcmode_b.setCharacterSize(48);
+
+
 	
 	while (window.isOpen())
 	{
@@ -310,7 +261,7 @@ int main()
 				window.close();
 
 		}
-		
+	
 		// кнопка Play
 		if (play_b.navediaMouse(event, mousePositon)) {
 
@@ -325,14 +276,18 @@ int main()
 
 		if (play_b.pressed(event, mousePositon)) {
 
-			if (!main_window.loadFromFile("images/game_window.png")) return 4;
-			background.setTexture(&main_window);
+			if (!window_background.loadFromFile("images/mode_window.png")) return 4;
+			background.setTexture(&window_background);
+
+			back_b.setPosition(135, 85);
+			playermode_b.setPosition(317, 312);
+			pcmode_b.setPosition(317, 563);
 
 			play_b.setPosition(2000, 2000);
 			rules_b.setPosition(2000, 2000);
 		}
 
-		// кнопка rules
+		// кнопка Rules
 		if (rules_b.navediaMouse(event, mousePositon)) {
 
 			rules_b.setFillRacktengelColor(255, 216, 132);
@@ -346,17 +301,96 @@ int main()
 
 		if (rules_b.pressed(event, mousePositon)) {
 
-			if (!main_window.loadFromFile("images/rules_window.png")) return 4;
-			background.setTexture(&main_window);
+			if (!window_background.loadFromFile("images/rules_window.png")) return 4;
+			background.setTexture(&window_background);
 
+			back_b.setPosition(135, 85);
 			rules_b.setPosition(2000, 2000);
 			play_b.setPosition(2000, 2000);
 		}
 
+		// кнопка Back
+		if (back_b.navediaMouse(event, mousePositon)) {
+
+			back_b.setFillTextColor(255, 216, 132);
+
+		}
+		else
+		{
+			back_b.setFillTextColor(255, 255, 221);
+
+		}
+
+		if (back_b.pressed(event, mousePositon)) {
+
+			if (!window_background.loadFromFile("images/main_window.png")) return 4;
+			background.setTexture(&window_background);
+
+			back_b.setPosition(2000, 2000);  // убираем
+			play_b.setPosition(835, 611);    // возвращаем
+			rules_b.setPosition(835, 718);   // возвращаем
+		}
+
+		// кнопка Player vs Player
+
+		if (playermode_b.navediaMouse(event, mousePositon)) {
+
+			playermode_b.setFillRacktengelColor(255, 216, 132);
+			playermode_b.setFillTextColor(0, 0, 0);
+
+		}
+		else
+		{
+			playermode_b.setFillRacktengelColor(149, 165, 58);
+			playermode_b.setFillTextColor(255, 255, 221);
+
+		}
+
+		if (playermode_b.pressed(event, mousePositon)) {
+
+			if (!window_background.loadFromFile("images/game_window.png")) return 4;
+			background.setTexture(&window_background);
+
+			back_b.setPosition(135, 85);
+			pcmode_b.setPosition(2000, 2000);
+			playermode_b.setPosition(2000, 2000);
+			rules_b.setPosition(2000, 2000);
+			play_b.setPosition(2000, 2000);
+		}
+
+		// кнопка Player vs pc
+
+		if (pcmode_b.navediaMouse(event, mousePositon)) {
+
+			pcmode_b.setFillRacktengelColor(255, 216, 132);
+			pcmode_b.setFillTextColor(0, 0, 0);
+
+		}
+		else
+		{
+			pcmode_b.setFillRacktengelColor(149, 165, 58);
+			pcmode_b.setFillTextColor(255, 255, 221);
+
+		}
+
+		/*if (pcmode_b.pressed(event, mousePositon)) {
+
+			if (!window_background.loadFromFile("images/rules_window.png")) return 4;
+			background.setTexture(&window_background);
+
+			back_b.setPosition(135, 85);
+			rules_b.setPosition(2000, 2000);
+			play_b.setPosition(2000, 2000);
+		}*/
+
+
 		window.clear();
 		window.draw(background);
+
 		play_b.draw(window);
 		rules_b.draw(window);
+		back_b.draw(window);
+
 		window.display();
 	}
 
