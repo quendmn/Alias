dcd19accc4c9a00966e8b1c395b4a46bda25c538
Mikhail Biriukov <12212005b@gmail.com>
2023-08-07 13:46:56 +0300

    добавил алгоритм для словаря

--- alias/InputWordsAndClues.cpp
+++ alias/InputWordsAndClues.cpp
@@ -2,4 +2,42 @@
 
 void InputWordsAndClues(std::string* arr)
 {
+	const std::string fileName = "dictionary2.txt";
+	std::ifstream wordsRand(fileName);
+
+	if (!wordsRand.is_open())
+	{
+		//надо будет сделать вывод ошибки
+	}
+	else {
+		int line = rand() % 3;//ввести конечное кол-во строчек
+		std::string currentLine;
+		int counter = 0;
+
+		for (int i = 0; i < arr->size(); i++)//очищаем массив потому-что push_back() лишь добавляет элемент, но не заменяет
+		{
+			if (arr[i] != "")
+			{
+				arr[i] = "";
+			}
+		}
+
+		while (std::getline(wordsRand, currentLine) && ++counter <= line) {
+			if (counter == line)
+			{
+				int i = 0;
+				for (char splitString : currentLine)//так как string это массив char-ов мы проходимся по нему
+				{
+					if (splitString == *(",")) {	//если splitString равна запятой мы меняем индекс для заполнения другой ячейки другими подсказками
+						i++;
+						continue; //contune для того чтоб в слова/подсказки не записывалась запятая
+					}
+					arr[i].push_back(splitString);//записываем по смвольно так как проходимся char-ом а не целой строкой
+				}
+
+			}
+		}
+	}
+
+	wordsRand.close();
 }
--- alias/InputWordsAndClues.hpp
+++ alias/InputWordsAndClues.hpp
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef _INPUTWORDSANDCLUES HPP_
+
+#define _INPUTWORDSANDCLUES HPP_
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+void InputWordsAndClues(std::string* arr);
+
+#endif
+
+
+
+
