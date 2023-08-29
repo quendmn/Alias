d916d0a6859e66a0ed057c4cd43c5b604c1c99ac
sashaxxxxx <zoloto.alex08@yandex.ru>
2023-08-29 22:46:07 +0500

    -

--- alias/Start.cpp
+++ alias/Start.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <sstream>
 #include <list>
@@ -396,11 +397,15 @@ int Start(RenderWindow& window, std::vector<string> Words, int *arr)
 			ten.setPosition(2000, 2000);
 
 
-				PlayerOneWonText.setPosition(2000, 2000);
+			Winning_Text.setPosition(2000, 2000);
+			guessedTwo_b.setPosition(2000, 2000);
+
+
+				/*PlayerOneWonText.setPosition(2000, 2000);
 
 				PlayerTwoWonText.setPosition(2000, 2000);
 
-				FriendshipWonText.setPosition(2000, 2000);
+				FriendshipWonText.setPosition(2000, 2000);*/
 
 
 		}
