176f4e9962caf335c2f3ff4199acc4f77518d04b
Chernyh Pauline <xanolaaa@gmail.com>
2023-08-30 21:28:22 +0500

    --

--- alias/Start.cpp
+++ alias/Start.cpp
@@ -396,11 +396,11 @@ int Start(RenderWindow& window, std::vector<string> Words, int *arr)
 			ten.setPosition(2000, 2000);
 
 
-				PlayerOneWonText.setPosition(2000, 2000);
+				/*PlayerOneWonText.setPosition(2000, 2000);
 
 				PlayerTwoWonText.setPosition(2000, 2000);
 
-				FriendshipWonText.setPosition(2000, 2000);
+				FriendshipWonText.setPosition(2000, 2000);*/
 
 
 		}
